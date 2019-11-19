633c60e6a822e39e0b68e164f5b03162e7ccd271
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2019-11-19 16:34:06 -0300

    added new feature

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,55 +1,83 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable lines-between-class-members */
+/* eslint-disable react/state-in-constructor */
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
 import api from '../../services/api';
-import { Container, List, Header, Detail } from './styles';
+// import { Container, List, Header, Detail } from './styles';
 
 export default class User extends Component {
-  async getUsers() {
-    const response = await api.get('users');
+  state = {
+    users: [],
+  };
+  users = [];
+  async componentDidMount() {
+    const response = await api.get('/tickets/staff');
 
-    return response;
+    await response.data.map(c => this.getUserTasks(c));
+
+    this.setState({
+      users: this.users,
+    });
+
+    this.users.length = 0;
+  }
+
+  async getUsers() {
+    const response = await api.get('/tickets/staff');
+
+    response.data.map(c => this.getUserTasks(c));
+
+    this.setState({
+      users: this.users,
+    });
+
+    this.users.length = 0;
   }
 
   async getUserTasks(user) {
-    const response = await api.get(`tasksByUser/${user.id}`);
-
-    return response;
+    const response = await api.get(`tickets/staff/${user.staff_id}`);
+    const { data } = response;
+    const newUser = {
+      name: user.Staff.firstname,
+      lastName: user.Staff.lastname,
+      tickets: data,
+    };
+    this.users.push(newUser);
   }
 
   render() {
+    const { users } = this.state;
+
+    if (users.length < 1) {
+      return <h1>{users.length}</h1>;
+    }
     return (
-      <>
-        <Container>
-          Thiago
-          <List>
-            <Header>
-              Em aberto
-              <Detail>12</Detail>
-            </Header>
-            <Header>
-              Fechadas
-              <Detail>10</Detail>
-            </Header>
-            <Header>
-              Concluídas
-              <Detail>14</Detail>
-            </Header>
-            <Header>
-              Atualizar cliente
-              <Detail>8</Detail>
-            </Header>
-          </List>
-        </Container>
-        <Container>
-          Thiago
-          <List>
-            <Header>
-              Em aberto
-              <Detail>12</Detail>
-            </Header>
-          </List>
-        </Container>
-      </>
+      <div>
+        {users.map(user => (
+          <h1 key={user.name}>{user.name}</h1>
+        ))}
+      </div>
+      // <>
+      //   <Container>
+      //     Thiago
+      //     <List>
+      //       <Header>
+      //         Em aberto
+      //         <Detail>12</Detail>
+      //       </Header>
+      //     </List>
+      //   </Container>
+      //   <Container>
+      //     Thiago
+      //     <List>
+      //       <Header>
+      //         Em aberto
+      //         <Detail>12</Detail>
+      //       </Header>
+      //     </List>
+      //   </Container>
+      // </>
     );
   }
 }
