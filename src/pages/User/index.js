61908c7401e24e82cec7227edd7f99597f0e545f
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2019-11-22 12:15:28 -0300

    minor bugs correction

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,83 +1,76 @@
+/* eslint-disable operator-assignment */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+/* eslint-disable prefer-template */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable lines-between-class-members */
 /* eslint-disable react/state-in-constructor */
 /* eslint-disable class-methods-use-this */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
-// import { Container, List, Header, Detail } from './styles';
+import { Container, List, Header, Detail } from './styles';
 
-export default class User extends Component {
-  state = {
-    users: [],
-  };
-  users = [];
-  async componentDidMount() {
-    const response = await api.get('/tickets/staff');
+function User() {
+  const [users, setUsers] = useState([]);
+  let idLastObject;
+  let index = 0;
+  const usersGroup = [];
 
-    await response.data.map(c => this.getUserTasks(c));
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/tickets/staff');
 
-    this.setState({
-      users: this.users,
-    });
-
-    this.users.length = 0;
-  }
-
-  async getUsers() {
-    const response = await api.get('/tickets/staff');
-
-    response.data.map(c => this.getUserTasks(c));
-
-    this.setState({
-      users: this.users,
-    });
-
-    this.users.length = 0;
-  }
-
-  async getUserTasks(user) {
-    const response = await api.get(`tickets/staff/${user.staff_id}`);
-    const { data } = response;
-    const newUser = {
-      name: user.Staff.firstname,
-      lastName: user.Staff.lastname,
-      tickets: data,
-    };
-    this.users.push(newUser);
-  }
-
-  render() {
-    const { users } = this.state;
-
-    if (users.length < 1) {
-      return <h1>{users.length}</h1>;
+      setUsers(...users, response.data);
     }
-    return (
-      <div>
-        {users.map(user => (
-          <h1 key={user.name}>{user.name}</h1>
-        ))}
-      </div>
-      // <>
-      //   <Container>
-      //     Thiago
-      //     <List>
-      //       <Header>
-      //         Em aberto
-      //         <Detail>12</Detail>
-      //       </Header>
-      //     </List>
-      //   </Container>
-      //   <Container>
-      //     Thiago
-      //     <List>
-      //       <Header>
-      //         Em aberto
-      //         <Detail>12</Detail>
-      //       </Header>
-      //     </List>
-      //   </Container>
-      // </>
-    );
+    loadUsers();
+  }, []);
+
+  function createUsers(object) {
+    if (!idLastObject) {
+      const newUser = {
+        staff_id: object.staff_id,
+        name: object.Staff.firstname + ' ' + object.Staff.lastname,
+        tickets: [object.TicketStatus],
+      };
+      usersGroup.push(newUser);
+      index = index + 1;
+    } else if (idLastObject === object.staff_id) {
+      usersGroup[index - 1].tickets.push(object.TicketStatus);
+    } else {
+      const newUser = {
+        staff_id: object.staff_id,
+        name: object.Staff.firstname + ' ' + object.Staff.lastname,
+        tickets: [object.TicketStatus],
+      };
+      usersGroup.push(newUser);
+      index = index + 1;
+    }
+
+    idLastObject = object.staff_id;
   }
+
+  users.map(user => createUsers(user));
+
+  console.log(usersGroup);
+
+  return (
+    <>
+      {usersGroup.map(user => (
+        <Container key={user.staff_id}>
+          {user.name}
+          <List>
+            {user.tickets.map(ticket => (
+              <Header>
+                {ticket.name}
+                <Detail status={ticket.name}>{ticket.count}</Detail>
+              </Header>
+            ))}
+          </List>
+        </Container>
+      ))}
+    </>
+  );
 }
+
+export default User;
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -1,6 +1,51 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const handleStatusColor = status => {
+  switch (status) {
+    case 'Em aberto':
+      return 'color: #9cf573;';
+    case 'Em análise':
+      return 'color: #ffb482;';
+    case 'Pendentes':
+      return 'color: #333';
+    case 'Em desenvolvimento':
+      return 'color: #4d91ff;';
+    case 'Concluído':
+      return 'color: #c878e3;';
+    case 'Atualizar cliente':
+      return 'color: #ffff63;';
+    case 'Aguardando cliente':
+      return 'color: #d97c5d';
+    case 'Suspenso':
+      return 'color: #fff';
+    default:
+      return 'color: #333;';
+  }
+};
+const handleStatusBackgroundColor = status => {
+  switch (status) {
+    case 'Em aberto':
+      return 'background: #007a2f;';
+    case 'Em análise':
+      return 'background: #db7900;';
+    case 'Pendentes':
+      return 'background: #ff0000;';
+    case 'Em desenvolvimento':
+      return 'background: #002f7a;';
+    case 'Concluído':
+      return 'background: #a300d9;';
+    case 'Atualizar cliente':
+      return 'background: #b5b500;';
+    case 'Aguardando cliente':
+      return 'background: #ba3306';
+    case 'Suspenso':
+      return 'background: #000000';
+    default:
+      return 'background: #93a79f;';
+  }
+};
+
+export const Container = styled.ul`
   text-align: center;
   padding: 10px;
   font-weight: bold;
@@ -10,7 +55,7 @@ export const Container = styled.div`
   margin-bottom: 15px;
 `;
 
-export const List = styled.div`
+export const List = styled.li`
   font-weight: normal;
   display: grid;
   text-align: left;
@@ -30,7 +75,7 @@ export const Detail = styled.div`
   text-align: center;
   width: 50px;
   height: 50px;
-  background: #faa200;
+  ${({ status }) => handleStatusBackgroundColor(status)};
   border-radius: 50%;
-  color: #000;
+  ${({ status }) => handleStatusColor(status)};
 `;
